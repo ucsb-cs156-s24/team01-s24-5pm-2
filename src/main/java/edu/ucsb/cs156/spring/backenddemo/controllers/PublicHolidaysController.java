24b5247b6a12602cf9cb8198da2c3e181ffa2056
Abhishek Ambastha <abhishekambastha@Abhisheks-MBP.hsd1.ca.comcast.net>
2024-04-22 19:27:36 -0700

    aa - made changes to fix controller test and service

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/PublicHolidaysController.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/PublicHolidaysController.java
@@ -1,37 +1,40 @@
 package edu.ucsb.cs156.spring.backenddemo.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
-
-import edu.ucsb.cs156.spring.backenddemo.services.EarthquakeQueryService;
 import edu.ucsb.cs156.spring.backenddemo.services.PublicHolidayQueryService;
 import lombok.extern.slf4j.Slf4j;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 
-@Tag(name="Public Holiday Query Info from https://date.nager.at/Api")
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name="Public holidays from https://date.nager.at/Api")
 @Slf4j
 @RestController
 @RequestMapping("/api/publicholidays")
-
 public class PublicHolidaysController {
-    ObjectMapper oM = new ObjectMapper();
+
+    ObjectMapper mapper = new ObjectMapper();
+
     @Autowired
     PublicHolidayQueryService publicHolidayQueryService;
-    @Operation(summary = "Get public holidays for a given year and country")
+
+    @Operation(summary="Recieve the 2 letter code", description ="Data of country uploaded")
     @GetMapping("/get")
     public ResponseEntity<String> getPublicHolidays(
-        @Parameter(name="year", description="year", example="2024") @RequestParam String year,
-        @Parameter(name="countryCode", description="2 letter country code", example="UK") @RequestParam String countryCode
+        @Parameter(name="year", description="requested year") @RequestParam String year,
+        @Parameter(name="countryCode", description="countryCode of country") @RequestParam String countryCode
     ) throws JsonProcessingException {
-        log.info("getHolidays: year={} countryCode={}", year, countryCode);
         String result = publicHolidayQueryService.getJSON(year, countryCode);
         return ResponseEntity.ok().body(result);
     }
